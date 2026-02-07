eae4fbfb891cba14d48f04ef0243231c4fcae597
Skygater27 <benjamin.jensen@gmx.net>
2026-02-07 17:39:53 +0100

    Update sw.js

--- schlafen_PWA_app/sw.js
+++ schlafen_PWA_app/sw.js
@@ -1,9 +1,26 @@
-const cacheName = 'schlafen-app-v1';
-const assets = ['./', './index.html', './style.css', './app.js'];
+const cacheName = 'schlafen-app-v2';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './img/icon-180.png',
+  './img/icon-192.png',
+  './img/icon-512.png'
+];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open('v1').then(cache => cache.addAll(['/', 'index.html', 'style.css', 'app.js'])));
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
 });
+
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
