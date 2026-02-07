7209a059afb3a04b1948cb276c443f8a2d155ee6
Skygater27 <benjamin.jensen@gmx.net>
2026-02-07 22:40:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const minuten = jetzt.getMinutes();
 
 // Wir rechnen alles in Minuten um (Stunden * 60 + Minuten)
 const aktuelleGesamtMinuten = stunden * 60 + minuten;
-const zielZeitPunkt = 22 * 60 + 10; // 22:10 Uhr in Minuten = 1330
+const zielZeitPunkt = 22 * 60 + 45; // 22:45 Uhr in Minuten = 1330
 
 if (aktuelleGesamtMinuten <= zielZeitPunkt) {
     document.body.style.color = "white";
 } else {
     document.body.style.color = "grey";
 }
+
 
 
