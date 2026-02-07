509b766ed3c40ef02c2b8103595ab99ad317b1a4
Skygater27 <benjamin.jensen@gmx.net>
2026-02-07 22:35:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const zielZeitPunkt = 22 * 60 + 10; // 22:10 Uhr in Minuten = 1330
 if (aktuelleGesamtMinuten <= zielZeitPunkt) {
     document.body.style.color = "white";
 } else {
-    document.body.style.color = "black";
+    document.body.style.color = "grey";
 }
+
 
