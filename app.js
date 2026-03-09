c3e308af3f4a2f23c82fd8a7efa4b9e30c32275f
Skygater27 <benjamin.jensen@gmx.net>
2026-03-09 19:50:08 +0100

    Update app.js
    Added Grey Time for 4:15

--- app.js
+++ app.js
@@ -3,24 +3,25 @@ function checkTime() {
     const stunden = jetzt.getHours();
     const minuten = jetzt.getMinutes();
     
-    // Wir rechnen alles in Minuten um (Stunden * 60 + Minuten)
     const aktuelleGesamtMinuten = stunden * 60 + minuten;
-    const aufstehZeit = 5 * 60 + 0; // 6:00 Uhr in Minuten = 360
+    const aufstehZeit = 5 * 60 + 0;
+    const grauZeit = 4 * 60 + 15;
     const schlafensZeit = 21 * 60 + 30;
 
     const body = document.body;
-    // Aufstehen
+    
     if (aktuelleGesamtMinuten >= aufstehZeit && aktuelleGesamtMinuten < schlafensZeit) {
-        body.style.backgroundColor = "#ffffff";
-        console.log("White");
-    } else { // Schlafen   
-        body.style.backgroundColor = "#000000";
-        console.log("Black");
-    }   
-}    
+        body.style.backgroundColor = "#ffffff"; // Weiß
+    } else if (aktuelleGesamtMinuten >= grauZeit) {
+        body.style.backgroundColor = "#888888"; // Grau
+    } else {
+        body.style.backgroundColor = "#000000"; // Schwarz
+    }
+} 
 
-// WICHTIG: Die Funktion ausführen, sobald die Seite geladen hat
+
 window.addEventListener('DOMContentLoaded', () => {
     checkTime();
 });
+
 
