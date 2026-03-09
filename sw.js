a8a4546c15b3b10ef836bce22fc83b61d7d8e345
Skygater27 <benjamin.jensen@gmx.net>
2026-03-09 19:50:47 +0100

    Update sw.js
    V 16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'schlafen-app-v15';
+const cacheName = 'schlafen-app-v16';
 const assets = [
   './',
   './index.html',
@@ -48,6 +48,7 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
 
 
 
